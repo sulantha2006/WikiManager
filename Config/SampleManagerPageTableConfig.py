577130e7fa73200d144770b400dcd6f6a0f79452
sulantha <sulantha.s@gmail.com>
2013-12-02 05:03:32 -0500

    Added template_created var to pagetable

--- Config/SampleManagerPageTableConfig.py
+++ Config/SampleManagerPageTableConfig.py
@@ -7,6 +7,7 @@ entityColumnDictionary = {'wells': 'WELL_API_COUNTY_ID'}
 managerPageTableName = 'wikiManagerPages'
 managerPageTableColumns = dict(pageTitle='PAGE_NAME',
                                typeColumn='TYPE',
+                               templateCreated='TEMPLATE_CREATED',
                                sourceCreated='SOURCE_CREATED',
                                pageCreated='PAGE_CREATED',
                                lastMod='LAST_MOD')
