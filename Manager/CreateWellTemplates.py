9683d50e84d28ae96856460429ba7c555c9b7df7
sulantha <sulantha.s@gmail.com>
2013-12-27 18:56:37 -0500

    Minor spelling correction.

--- Manager/CreateWellTemplates.py
+++ Manager/CreateWellTemplates.py
@@ -2,8 +2,8 @@ __author__ = 'sulantha'
 from FileUtil.WellTableTemplateBuilder import WellTableTemplateBuilder
 from DBUtil.PageTableAccesor import PageTableAccessor
 
-pageTableAccesor = PageTableAccessor()
-newWellIDs = [x[0] for x in pageTableAccesor.getNewWells()]
+pageTableAccessor = PageTableAccessor()
+newWellIDs = [x[0] for x in pageTableAccessor.getNewWells()]
 
 for wellID in newWellIDs:
     tableTemplateBuilder = WellTableTemplateBuilder(wellID)
